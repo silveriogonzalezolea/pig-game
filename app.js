83cd7cfbd46929fd86a93f8099e77ef3a235b647
Silveiro Gonzalez <sgoolea1993@gmail.com>
2018-06-18 18:46:00 -0500

    added user functionality

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ var scores, roundScore, activePlayer;
 
 scores =[0, 0 ];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 //console.log(dice);
 
-//document.querySelector('#current-'+ activePlayer).textContent = dice;
+
 
 // setter
 //document.querySelector('#current-' + activePlayer).innerHTML = //'<em>' + dice + '</em>';
@@ -29,6 +29,10 @@ activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
@@ -39,4 +43,45 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 	diceDOM.src = 'dice-' + dice + '.png';
 
 
+	if (dice !== 1){
+
+		roundScore += dice;
+		document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+
+	} else {
+		// next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+	// red dot active player
+		//document.querySelector('.player-0-panel').classList.remove('active');
+
+		//document.querySelector('.player-1-panel').classList.add('active')
+
+		// hide dice again
+		document.querySelector('.dice').style.display = 'none';
+
+	}
+
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+
+	// Add current score to global score
+
+
+	// Update the UI
+
+
+	// Check if player won game
+
+});
+
